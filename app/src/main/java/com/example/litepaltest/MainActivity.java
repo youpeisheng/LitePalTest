d7a5fbdeaebea07daae6617a3f68705f7a644911
youpeisheng <1426890389@qq.com>
2019-11-18 16:31:32 +0800

    Initial commit

---
+++ app/src/main/java/com/example/litepaltest/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.litepaltest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import org.litepal.crud.DataSupport;
+import org.litepal.tablemanager.Connector;
+import org.litepal.util.Const;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button createDatabase=(Button) findViewById(R.id.create_database);
+        createDatabase.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Connector.getDatabase();
+
+            }
+        });
+    }
+}
